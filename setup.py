bf8510239fb903cfe53fa363282aa796e8ef7740
Amirul Sadikin <37245308+aiamirul@users.noreply.github.com>
2025-06-05 10:22:05 +0800

    dd

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='sampah',
-    version='0.1.0',
+    name="sampah",  # your package name here
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[],
-    author='Amirul Sadikin',
-    author_email='ai.amirul.sadikin@gmail.com',
-    description='SAMPAH – Smart Annotation Management Platform for Adaptive Handling',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/yourrepo',
+    author="Your Name",
+    author_email="you@example.com",
+    description="Short description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/SAMPAH",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.6",
 )
